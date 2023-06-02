07a48bd00403bec44e9d0db5b6aeea07f504c129
cudhnyuh <phamhuynhduc805@gmail.com>
2023-06-03 04:40:08 +0700

    Update Player Functionality

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -7,6 +7,7 @@ import { Song } from "@/types";
 import { useUser } from "@/hooks/useUser";
 import MediaItem from "@/components/MediaItem";
 import LikeButton from "@/components/LikeButton";
+import useOnPlay from "@/hooks/useOnPlay";
 
 
 interface LikedContentProps {
@@ -18,6 +19,8 @@ const LikedContent: React.FC<LikedContentProps> = ({
 }) => {
   const router = useRouter();
   const { isLoading, user } = useUser();
+
+  const onPlay = useOnPlay(songs);
 
   useEffect(() => {
     if (!isLoading && !user) {
@@ -49,7 +52,8 @@ const LikedContent: React.FC<LikedContentProps> = ({
         >
           <div className="flex-1">
             <MediaItem 
-                onClick={() => {}} data={song} 
+                onClick={(id: string) => onPlay(id)} 
+                data={song} 
             />
           </div>
           <LikeButton songId={song.id} />
